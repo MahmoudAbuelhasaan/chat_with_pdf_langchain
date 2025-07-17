ca277e28be3336a89c3299a4be26c8810304edfe
mahmoud abuelhassa <mahmoud.abuelhassan@outlook.com>
2025-07-17 15:01:50 +0300

    add auth system

--- app/auth/apis.py
+++ app/auth/apis.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,request,flash,url_for
-from flask_login import current_user,login_required
+from flask_login import current_user,login_required,login_user,logout_user
 from app.auth.forms import LoginForm, RegisterForm
 from app.models import User
 from app import db
@@ -7,8 +7,17 @@ from app.auth import bp
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash('You are loged in. ')   
+            return redirect('/chat/chat')     
 
-    return render_template('auth/login.html')
+    return render_template('auth/login.html',form=form)
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from app.auth.urls import auth_routs_bp
-
-
-def register_routes(app):
-
-    app.register_blueprint(auth_routs_bp,url_prefix='/auth')
